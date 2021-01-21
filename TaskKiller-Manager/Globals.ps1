cee826dbfee1c8ecfa457c0bc77b5d8640454d1e
TheNFischer <fischer.nico@outlook.com>
2021-01-21 16:08:31 +0100

    added table functionality

--- TaskKiller-Manager/Globals.ps1
+++ TaskKiller-Manager/Globals.ps1
@@ -2,6 +2,35 @@
 # Declare Global Variables and Functions here
 #--------------------------------------------
 
+## - [ Section to initialize DataTable objects] - ##
+## - Create DataTable:
+$processTable = New-Object System.Data.DataTable;
+
+## - Defining DataTable object columns and rows properties:
+# - Column1 = "ID".
+$column = New-Object System.Data.DataColumn;
+$column.DataType = [System.Type]::GetType("System.Int32");
+$column.ColumnName = "ID";
+$processTable.Columns.Add($column);
+
+# - Column2 = "Process Name".
+$column = New-Object System.Data.DataColumn;
+$column.DataType = [System.Type]::GetType("System.String");
+$column.ColumnName = "ProcessName";
+$processTable.Columns.Add($column);
+
+# - Column3 = "CPU".
+$column = New-Object System.Data.DataColumn;
+$column.DataType = [System.Type]::GetType("System.String");
+$column.ColumnName = "CPU";
+$processTable.Columns.Add($column);
+
+# - Column4 = "Responding".
+$column = New-Object System.Data.DataColumn;
+$column.DataType = [System.Type]::GetType("System.Boolean");
+$column.ColumnName = "Responding";
+$processTable.Columns.Add($column);
+
 
 #Sample function that provides the location of the script
 function Get-ScriptDirectory
